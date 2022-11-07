0b2cdbafa67a103aace61db6f2dcdb9a5ece9f5a
Aris <aris.rosbach@hva.nl>
2022-11-07 14:59:11 +0100

    Add main.js duur attracties barchart

--- scripts/main.js
+++ scripts/main.js
@@ -6,101 +6,138 @@ import * as d3 from 'd3';
 
 console.log('Hello, world!');
 
-/* Assume we have a non-normalized dataset to start with */
+// Duur van disneyland attracties weergeven in barchart
+///////////////////////////////////////
+function getData() {
+	console.log('Grabbing new userdata...');
 
-const data = [
-	{
-		id: 1,
-		name: 'ROBERT',
-		kaas: false,
-		coords: {
-			lat: "52.3676",
-			long: "4.9041"
-		}
-	},
-	{
-		id: "2",
-		name: 'viNcent',
-		kaas: "true",
-		coords: {
-			lat: "52.3676",
-			long: "4.9041"
-		}
-	},
-	{
-		id: 3,
-		name: 'laura',
-		kaas: true,
-		coords: {
-			lat: "52.3676",
-			long: "4.9041"
-		}
-	},
-]
+	fetch('https://opensheet.elk.sh/1JTKk5zpB87MaeZzYJUnTSX2IsuHyLa8JF_l79Svn9P4/DisneylandParis')
+		.then(res => res.json())
+		.then(data => {
 
-// This assignment builds on the earlier assignment we did today. Create a table containing the above dataset, this time though, the dataset is a bit scuffed.
-// We need to normalize the data by addressing the following issues:
-// 1) The id is not always a number, convert it to an integer / number first.
-// 2) The name isn't normalized, random capitals appear. Change this string in a first letter capital and lowercase after that
-// 3) "kaas" is not always a boolean. Convert it to a string so we can print it in HTML
-// 4) "coords" is an object. If you try and print this, you'll get [object Object] or something.
-//We'll have to loop over the object and print a custom string using template literals.
 
-function transformArrOfObj() {
-  
-  let newData = data.map(item => {
-    
-    return {
-      id: parseInt(item.id),
-      name: item.name.charAt(0).toUpperCase() + item.name.slice(1).toLowerCase(),
-      kaas: String(item.kaas),
-      coords: item.coords.lat + " <br> " + item.coords.long
-    }
-  })
-  
-  console.log(newData);
-}
+				console.log(data);
 
-function generateTable() {
-// Continue using the code from thursday-1
-  let table = document.querySelector('table');
-  let tHeading = document.querySelector('thead tr');
-  let tBody = document.querySelector('tbody');
-  
-  
-// Volgensmij is dit niet de manier maar het werkt
-  let newData = data.map(item => {
-    
-    return {
-      id: parseInt(item.id),
-      name: item.name.charAt(0).toUpperCase() + item.name.slice(1).toLowerCase(),
-      kaas: String(item.kaas),
-      coords: item.coords.lat + " <br> " + item.coords.long
-    }
-  })
-  
-  
-  Object.keys(newData[0]).forEach(key => {
-      
-      let newElement = document.createElement('th');
-      newElement.textContent = key;
-      tHeading.appendChild(newElement);
-      
-    })
-  
-  newData.forEach(obj => {
-    
-    let tr = document.createElement('tr');
-    tBody.appendChild(tr);
-    
-    for (const [key, value] of Object.entries(obj)) {
-      
-      let td = document.createElement('td');
-      td.innerHTML = value;
-      tr.appendChild(td);
-    }
-  })
-}
+				const chartWidth = 1000
+				const chartHeight = 1000
 
-transformArrOfObj()
-generateTable()
+				const xScale = d3.scaleLinear()
+					.domain([0, d3.max(data, d => d.Duur)])
+					.range([0, chartWidth]);
+
+				const yScale = d3.scaleBand()
+					.domain(d3.map(data, d => d.Naam))
+					.range([0, chartHeight])
+					.paddingInner(0.05);
+
+				d3.select('#bars')
+					.selectAll('rect')
+					.data(data)
+					.join('rect')
+					.attr('height', 20) //yScale.bandwith())
+					.attr('width', d => xScale(d.Duur))
+					.attr('y', d => yScale(d.Naam))
+					.classed('animate __animated animate__headShake', 1)
+				//, () => Math.random() > 0.5
+				d3.select('#labels')
+					.selectAll('text')
+					.data(data)
+					.join('text')
+					.attr('y', d => yScale(d.Naam) + 15)
+					.text(d => d.Naam);
+				})
+
+			}
+
+			getData();
+
+// Aardbeving dataset met d3 in barchart weergeven
+///////////////////////////////////////
+// const dataSet = [{"Jaar":1991,"Aantal":1},{"Jaar":1992,"Aantal":0},{"Jaar":1993,"Aantal":4},{"Jaar":1994,"Aantal":7},{"Jaar":1995,"Aantal":4},{"Jaar":1996,"Aantal":8},{"Jaar":1997,"Aantal":7},{"Jaar":1998,"Aantal":6},{"Jaar":1999,"Aantal":5},{"Jaar":2000,"Aantal":8},{"Jaar":2001,"Aantal":2},{"Jaar":2002,"Aantal":4},{"Jaar":2003,"Aantal":14},{"Jaar":2004,"Aantal":6},{"Jaar":2005,"Aantal":11},{"Jaar":2006,"Aantal":21},{"Jaar":2007,"Aantal":12},{"Jaar":2008,"Aantal":11},{"Jaar":2009,"Aantal":18},{"Jaar":2010,"Aantal":16},{"Jaar":2011,"Aantal":29},{"Jaar":2012,"Aantal":20},{"Jaar":2013,"Aantal":30},{"Jaar":2014,"Aantal":20},{"Jaar":2015,"Aantal":23},{"Jaar":2016,"Aantal":13},{"Jaar":2017,"Aantal":18},{"Jaar":2018,"Aantal":15},{"Jaar":2019,"Aantal":11},{"Jaar":2020,"Aantal":17}]
+
+// const chartWidth = 700
+// const chartHeight = 800
+
+// const xScale = d3.scaleLinear()
+// 	.domain([0, d3.max(dataSet, d => d.Aantal)])
+// 	.range([0, chartWidth]);
+
+// const yScale = d3.scaleBand()
+// 	.domain(d3.map(dataSet, d => d.Jaar))
+// 	.range([0, chartHeight])
+//   .paddingInner(0.05);
+
+// d3.select('#bars')
+//   .selectAll('rect')
+//   .data(dataSet)
+//   .join('rect')
+//   .attr('height', 25) //yScale.bandwith())
+//   .attr('width', d => xScale(d.Aantal))
+//   .attr('y', d => yScale(d.Jaar))
+//   .classed('animate __animated animate__headShake',1)
+// //, () => Math.random() > 0.5
+// d3.select('#labels')
+//   .selectAll('text')
+//   .data(dataSet)
+//   .join('text')
+//   .attr('y', d => yScale(d.Jaar) + 15)
+//   .text(d => d.Jaar);
+
+
+// Disneypark data weergeven in tabel
+///////////////////////////////////////
+// function getData() {
+// 	console.log('Grabbing new userdata...');
+
+// 	fetch('https://opensheet.elk.sh/1JTKk5zpB87MaeZzYJUnTSX2IsuHyLa8JF_l79Svn9P4/DisneylandParis')
+// 		.then(res => res.json())
+// 		.then(data => {
+
+// 			console.log(data);
+
+// 			function generateTable() {
+
+// 				// There are a couple steps we need to take, first, we need to select the table, table heading and table body and save them to a variable
+
+// 				let table = document.querySelector('table');
+// 				let tHeading = document.querySelector('thead tr');
+// 				let tBody = document.querySelector('tbody');
+
+// 				// First, we'll generate a row of table headings, we need to grab the keys from all the objects, not the values! We can achieve this by using the Object.keys(data[0]) method of the native Object. 
+// 				// It returns an array of all keys an object contains. We can loop over that array using forEach(); It's up to you to find out how then to generate the corresponding HTML.
+
+// 				Object.keys(data[0]).forEach(key => {
+
+// 					let newElement = document.createElement('th');
+// 					newElement.textContent = key;
+// 					tHeading.appendChild(newElement);
+
+// 				})
+
+// 				// Your HTML should now display the headers in a <th></th> structure.
+
+// 				//After this, we can loop over the amount of objects inside of the array (looping over an array of objects can be useful here, for...of). 
+// 				// For every entry (forEach()) we want to create a new row (<tr>/tr>) and append three datapoints (<td>) inside of it containing the id, name and kaas.
+
+// 				data.forEach(obj => {
+
+// 					let tr = document.createElement('tr');
+// 					tBody.appendChild(tr);
+
+// 					for (const [key, value] of Object.entries(obj)) {
+
+// 						let td = document.createElement('td');
+// 						td.textContent = value;
+// 						tr.appendChild(td);
+// 					}
+// 				})
+
+// 			}
+
+// 			generateTable();
+
+// 		})
+
+// }
+
+// getData();
